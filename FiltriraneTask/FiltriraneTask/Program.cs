49f09b29131d3eed6c03d6d22c5e08a02764d456
Siyana <s.ivanova2020@pmg-pernik.com>
2024-11-19 11:10:23 +0200

    First

---
+++ FiltriraneTask/FiltriraneTask/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FiltriraneTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+            ;
+            numbers.RemoveAll(i => i < 0);
+
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+    }
+}
---
+++ ListTask/ListTask/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ListTask
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+            double sum = numbers.Sum();
+            double average = sum / numbers.Count;
+
+            average = Math.Round(average, 2);
+
+            Console.WriteLine($"Sum={sum}; Average={average}");
+        }
+    }
+}
